26eeb4e881e8ed8c0193bcf8b81d46fd04649c0a
Shiva Sankar J <jshivasankar607@gmail.com>
2026-07-25 13:06:07 +0530

    Updated the code

--- tests/UI_Test/steps/login.steps.ts
+++ tests/UI_Test/steps/login.steps.ts
@@ -6,7 +6,7 @@ const { Given, When, Then } = createBdd(test);
 
 Given('I navigate to {string}', async ({ logInPage }, url) => {
   await logInPage.navigateToURL(url);
-  console.log('Hey 1');
+  console.log('This is github page branch report');
 });
 
 Given('I click on My account', async ({ logInPage }) => {
